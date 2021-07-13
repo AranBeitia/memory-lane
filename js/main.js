3d10b997aeeebd2159c6843168c7ffac23aed742
Cherrerotinoco <c.herrerotinoco@gmail.com>
2021-07-13 12:44:58 +0200

    Layout gallery

--- js/main.js
+++ js/main.js
@@ -9,15 +9,35 @@
 let userName = '';
 let mainContent = document.querySelector('.main-content');
 
-let formHTML = `<form id="user-form" class="form-elements">
-<h1 class="form-screen">Choose a user name</h1>
-<label for="name">Username</label>
-<input type="text" id="uname" name="name" maxlength="8" minlength="1" pattern="[a-zA-Z]{1,8}"/>
-<input type="submit" value="Start" />
+let formHTML = `
+<form id="user-form" class="form-elements">
+    <h1 class="form-screen">Choose a user name</h1>
+    <label for="name">Username</label>
+    <input type="text" id="uname" name="name" maxlength="8" minlength="1" pattern="[a-zA-Z]{1,8}"/>
+    <input type="submit" value="Start" />
 </form>
 `; 
 
-let galleryHTML = `<p>Im gallery</p>`; 
+let galleryHTML = `
+<section class="grid">
+    <img src="https://images.squarespace-cdn.com/content/v1/56a739dbb204d5c253feacd8/1455801509563-EN0CZLQAK6Z6NCIXOP09/img.jpg" alt="">
+    <img src="https://images.squarespace-cdn.com/content/v1/56a739dbb204d5c253feacd8/1455801509563-EN0CZLQAK6Z6NCIXOP09/img.jpg" alt="">
+    <img src="https://images.squarespace-cdn.com/content/v1/56a739dbb204d5c253feacd8/1455801509563-EN0CZLQAK6Z6NCIXOP09/img.jpg" alt="">
+    <img src="https://images.squarespace-cdn.com/content/v1/56a739dbb204d5c253feacd8/1455801509563-EN0CZLQAK6Z6NCIXOP09/img.jpg" alt="">
+    <img src="https://images.squarespace-cdn.com/content/v1/56a739dbb204d5c253feacd8/1455801509563-EN0CZLQAK6Z6NCIXOP09/img.jpg" alt="">
+    <img src="https://images.squarespace-cdn.com/content/v1/56a739dbb204d5c253feacd8/1455801509563-EN0CZLQAK6Z6NCIXOP09/img.jpg" alt="">
+    <img src="https://images.squarespace-cdn.com/content/v1/56a739dbb204d5c253feacd8/1455801509563-EN0CZLQAK6Z6NCIXOP09/img.jpg" alt="">
+    <img src="https://images.squarespace-cdn.com/content/v1/56a739dbb204d5c253feacd8/1455801509563-EN0CZLQAK6Z6NCIXOP09/img.jpg" alt="">
+    <img src="https://images.squarespace-cdn.com/content/v1/56a739dbb204d5c253feacd8/1455801509563-EN0CZLQAK6Z6NCIXOP09/img.jpg" alt="">
+    <img src="https://images.squarespace-cdn.com/content/v1/56a739dbb204d5c253feacd8/1455801509563-EN0CZLQAK6Z6NCIXOP09/img.jpg" alt="">
+    <img src="https://images.squarespace-cdn.com/content/v1/56a739dbb204d5c253feacd8/1455801509563-EN0CZLQAK6Z6NCIXOP09/img.jpg" alt="">
+    <img src="https://images.squarespace-cdn.com/content/v1/56a739dbb204d5c253feacd8/1455801509563-EN0CZLQAK6Z6NCIXOP09/img.jpg" alt="">
+    <img src="https://images.squarespace-cdn.com/content/v1/56a739dbb204d5c253feacd8/1455801509563-EN0CZLQAK6Z6NCIXOP09/img.jpg" alt="">
+    <img src="https://images.squarespace-cdn.com/content/v1/56a739dbb204d5c253feacd8/1455801509563-EN0CZLQAK6Z6NCIXOP09/img.jpg" alt="">
+    <img src="https://images.squarespace-cdn.com/content/v1/56a739dbb204d5c253feacd8/1455801509563-EN0CZLQAK6Z6NCIXOP09/img.jpg" alt="">
+    <img src="https://images.squarespace-cdn.com/content/v1/56a739dbb204d5c253feacd8/1455801509563-EN0CZLQAK6Z6NCIXOP09/img.jpg" alt="">
+</section>
+`; 
 
 
 function buildLayout(html) {
@@ -32,6 +52,7 @@ function buildLayout(html) {
     // Init userForm
     buildLayout(formHTML); 
     
+    // Form on submit
     let userForm = document.querySelector('#user-form');
     userForm.addEventListener('submit', e => {
         e.preventDefault();
