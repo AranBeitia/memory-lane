d2212a4703fc5bd2ed3ef057e8d023b17f50e650
AranBeitia <arantza.beitia@gmail.com>
2021-07-14 12:37:12 +0200

    changes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { buildLayout, galleryHTML, formHTML } from './layout.js'
 import { userName } from './form.js'
+import { images } from './game.js'
 
 // let userName = '';
 let mainContent = document.querySelector('.main-content');
